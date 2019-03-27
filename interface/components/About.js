5fb8afe7029f20d55411c991ccef7ffde561f6b0
hackyminer <hackyminer@gmail.com>
2019-03-28 05:35:00 +0900

    fix publicSettings

--- interface/components/About.js
+++ interface/components/About.js
@@ -5,7 +5,7 @@ class About extends React.Component {
     const appIconPath = `file://${window.dirname}/icons/${
       window.mistMode
     }/icon2x.png`;
-    const appName = window.mistMode === 'mist' ? 'Mist' : 'Ethereum Wallet';
+    const appName = window.mistMode === 'mist' ? 'Mist' : publicSettings.walletName || 'Ethereum Wallet';
 
     return (
       <div className="row popup-windows about">
@@ -29,11 +29,11 @@ class About extends React.Component {
             License {window.mist.license}
             <br />
             GitHub{' '}
-            <a href="https://github.com/ethereum/mist" target="_blank">
-              github.com/ethereum/mist
+            <a href="{publicSettings.homepage}" target="_blank">
+              {publicSettings.homepage}
             </a>
           </p>
-          <small>Copyright 2018 Ethereum Foundation</small>
+          <small>{publicSettings.copyright}</small>
         </div>
       </div>
     );
